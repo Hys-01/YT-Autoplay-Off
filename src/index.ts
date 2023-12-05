398fc0f93f46a92729e10d45a81374ef864992a0
Brian Hwang <hbros2004@gmail.com>
2023-12-05 19:19:05 +1100

    working on actual script to inject

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,28 @@
 function isInPlaylist(): boolean { 
+    /* 
+    checks if the current URL is in/part of a youtube playlist clicked on by the user. 
+    DOES NOT check if current url video is part of any playlist created by anyone
+    */
     return window.location.href.includes("list="); 
 }
 
+function stopAutoplay() { 
+    /* 
+    stops loading the next video in the playlist
+    */
+    let script = document.createElement("script"); 
+    script.type = "text/javascript"; // compiled into js
+
+    // the actual script
+    script.innerText = 
+        ` 
+    
+        `;
+
+}
+
+
+
 console.log("MY SCRIPT!", isInPlaylist());
+
+
