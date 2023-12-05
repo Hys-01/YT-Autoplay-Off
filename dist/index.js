65471e1ac3d631b1fd8e777b09418f557f9ebb8f
Brian Hwang <hbros2004@gmail.com>
2023-12-05 21:15:02 +1100

    compiled to js

--- dist/index.js
+++ dist/index.js
@@ -1,5 +1,36 @@
+"use strict";
 function isInPlaylist() {
+    /*
+    checks if the current URL is in/part of a youtube playlist clicked on by the user.
+    DOES NOT check if current url video is part of any playlist created by anyone
+    */
     return window.location.href.includes("list=");
 }
-console.log("MY SCRIPT!", isInPlaylist());
+function stopAutoplay() {
+    /*
+    stops loading the next video in the playlist
+    */
+    let script = document.createElement("script");
+    script.type = "text/javascript"; // compiled into js
+    // the actual script
+    script.innerText =
+        ` 
+        (function() {
+            function noAutoAdvance() {
+                const ypm = document.querySelector('yt-playlist-manager');
+                if (ypm) {
+                    ypm.canAutoAdvance_ = false;
+                }
+            }
+            noAutoAdvance();
+            setInterval(noAutoAdvance, 500); // Set a default frequency
+        })();
+        `;
+}
+function run() {
+    if (isInPlaylist()) {
+        stopAutoplay();
+    }
+}
+console.log("MY SCRIPT!", run());
 //# sourceMappingURL=index.js.map
