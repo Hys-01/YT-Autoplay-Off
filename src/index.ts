c0b47ab7fdfd119dada81bc6e98b257ed7b30951
Brian Hwang <hbros2004@gmail.com>
2023-12-07 00:03:55 +1100

    some documentation

--- src/index.ts
+++ src/index.ts
@@ -7,21 +7,25 @@ function isInPlaylist(): boolean {
 }
 
 
-function Autoplay_Off() { 
-    if (isInPlaylist()) { 
+function Turn_Autoplay_Off() { 
+
+    // check if current video is in playlist
+    if (isInPlaylist()) {
+        
+        // make a script, and append anonymous function to document, to asyncronously set AutoAdvance property to false, and to repeat at 0.5s intervals
         let script = document.createElement("script"); 
         script.appendChild(document.createTextNode('(' + function() { 
-            function noAutoAdvance() {
+            function AutoAdvance_Off() {
                 const manager: any = document.querySelector('yt-playlist-manager');
                 if (manager) { 
                     manager.canAutoAdvance_ = false;
                 }
             }
-            setInterval(noAutoAdvance, 500);
+            setInterval(AutoAdvance_Off, 500);
         }.toString() + ')()'));
         document.body.appendChild(script);
     }   
 }
-console.log("MY SCRIPT!", Autoplay_Off());
+console.log("MY SCRIPT!", Turn_Autoplay_Off());
 
 
