26dda1647d47f0d044702af8a6c41cb450fa0c84
Brian Hwang <hbros2004@gmail.com>
2023-12-08 15:35:40 +1100

    it works! added credit

--- src/inject.ts
+++ src/inject.ts
@@ -1,6 +1,11 @@
+//inject.ts
+// CREDIT: stackbykumbi   stackoverflow https://stackoverflow.com/questions/67439012/chrome-extension-manifest-v3-content-security-policy
+
 function injectScript(source_script: any) {
+    // externally inject to avoid content policy YT on manifest v3
     const script = document.createElement('script') 
     script.src = source_script
+
     script.onload = () => {    // = function() {} isnt working using this keyword
         script.remove();    // should remove it from document after
     }
